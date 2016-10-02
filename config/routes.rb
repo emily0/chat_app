f94e1755a5e980db9c3d9cc6dded394a1e0ff344
yumin0 <yumin3991@gmail.com>
2016-10-02 21:05:35 +0800

    adjust layout and add styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :chat_rooms, only: [:new, :create, :show, :index]
-  root 'chat_rooms#index'
+  root 'welcome#index'
   mount ActionCable.server => '/cable'
 end
