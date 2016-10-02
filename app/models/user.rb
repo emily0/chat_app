032ba6bbd59c659b5cf2fda2411b1d8ab920ddae
yumin0 <yumin3991@gmail.com>
2016-10-02 16:09:26 +0800

    Employ mew methods in partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def name
+    email.split('@')[0]
+  end
 end
