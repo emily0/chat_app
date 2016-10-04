db73a61455005a3d67dda6ef8ca1072766c9312f
yumin0 <yumin3991@gmail.com>
2016-10-04 09:15:43 +0800

    add styling and partial pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def programming
+  end
+
+  def marketing
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :chat_rooms, only: [:new, :create, :show, :index]
+
+  get 'programming', to: 'pages#programming'
+  get 'marketing', to: 'pages#marketing'
+
   root 'welcome#index'
   mount ActionCable.server => '/cable'
 end
